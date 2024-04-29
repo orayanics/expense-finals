01bc611ce579c4d3df2824a123d8d2706c5d1db8
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-04-30 01:01:04 +0800

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 export default function Dashboard() {
   const navigate = useNavigate();
 
+  const user = JSON.parse(localStorage.getItem("auth"));
+
   const logout = async () => {
     await signOut(auth);
     localStorage.clear("auth");
@@ -12,9 +14,20 @@ export default function Dashboard() {
   };
 
   return (
-    <div>
-      <h3>Dashboard</h3>
-      <button onClick={logout}>Logout</button>
-    </div>
+    <>
+      <div>
+        <h3>Dashboard</h3>
+        <button onClick={logout}>Logout</button>
+      </div>
+
+      {
+        user && (
+          <div>
+            <h4>{user.name}</h4>
+            <img src={user.photo} alt={user.name} />
+          </div>
+        )
+      }
+    </>
   );
 }
