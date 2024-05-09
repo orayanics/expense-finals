a7fd1e42d66b68c6e651bea36b42f033c5237b25
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-05-09 17:24:36 +0800

    faq accordion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,4 @@
 import "../styles/routes.css";
-
 function Contact() {
   return (
     <div className="contact">
--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,4 +1,5 @@
 import "../styles/routes.css";
+import { Accordion } from "react-bootstrap";
 
 function Faqs() {
   return (
@@ -18,46 +19,69 @@ function Faqs() {
       <main className="App-main">
         <section className="App-faq">
           <h2>Frequently Asked Questions</h2>
-          <article>
-            <h3>
-              1. What is an expense tracking website, and why should I use one?
-            </h3>
-            <p>
-              An expense-tracking website helps you monitor and manage your
-              spending by categorizing expenses, setting budgets, and generating
-              reports.
-            </p>
-          </article>
-          <article>
-            <h3>2. Is my financial information safe on this website?</h3>
-            <p>
-              Yes, we take the security of your financial information seriously.
-              We use encryption protocols, secure data storage, and other
-              measures to protect your data from unauthorized access.
-            </p>
-          </article>
-          <article>
-            <h3>
-              3. Can I sync my bank accounts and credit cards with this website?
-            </h3>
-            <p>
-              Yes, you can connect your bank accounts and credit cards to
-              automatically import transaction data.
-            </p>
-          </article>
-          <article>
-            <h3>4. Can I create custom categories for my expenses?</h3>
-            <p>
-              Yes, you can create custom categories to suit your specific needs.
-            </p>
-          </article>
-          <article>
-            <h3>5. How does budgeting work on this website?</h3>
-            <p>
-              You can set spending limits for different categories and receive
-              notifications when you're approaching or exceeding your budget.
-            </p>
-          </article>
+          <Accordion>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>
+                1. What is an expense tracking website, and why should I use
+                one?
+              </Accordion.Header>
+              <Accordion.Body>
+                An expense-tracking website helps you monitor and manage your
+                spending by categorizing expenses, setting budgets, and
+                generating reports.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+
+          <Accordion>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>
+                2. Is my financial information safe on this website?
+              </Accordion.Header>
+              <Accordion.Body>
+                Yes, we take the security of your financial information
+                seriously. We use encryption protocols, secure data storage, and
+                other measures to protect your data from unauthorized access.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+
+          <Accordion>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>
+                3. Can I sync my bank accounts and credit cards with this
+                website?
+              </Accordion.Header>
+              <Accordion.Body>
+                Yes, you can connect your bank accounts and credit cards to
+                automatically import transaction data.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+
+          <Accordion>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>
+                4. Can I create custom categories for my expenses?
+              </Accordion.Header>
+              <Accordion.Body>
+                Yes, you can create custom categories to suit your specific
+                needs.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+
+          <Accordion>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>
+                5. How does budgeting work on this website?
+              </Accordion.Header>
+              <Accordion.Body>
+                You can set spending limits for different categories and receive
+                notifications when you're approaching or exceeding your budget.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </section>
       </main>
       <a href="./Login" className="back-to-home">
