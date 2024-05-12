3dd400ded7b4b979c2e52044ffee2dd66738e07b
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-05-13 00:06:02 +0800

    responsive nav after login

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { successAlert } from '../utils/toastAlert'
-import '../styles/navigation.css';
+import { successAlert } from "../utils/toastAlert";
+import { useState } from "react";
+import "../styles/navigation.css";
 
 export default function Navigation() {
   const navigate = useNavigate();
+  const [showMobileList, setShowMobileList] = useState(false);
+
+  const showLinks = () => {
+    setShowMobileList(!showMobileList);
+  };
 
   const logout = async () => {
     await signOut(auth);
     localStorage.removeItem("auth");
-    successAlert("Logout successful")
+    successAlert("Logout successful");
     navigate("/");
   };
 
   return (
-    <nav>
+    <>
+      <nav>
       <div className="nav-container">
         <div className="logo-container">
           <img src="MONEYDADDY-2.png" alt="logo" className="logo"></img><p className="moneydaddy-tl">MoneyDaddy</p>
@@ -30,20 +37,29 @@ export default function Navigation() {
           <a href={`/Expense`} className="">
             <p className="nav-btn">Your Expenses</p>
           </a>
-          
-          {/* <a href={`/HowItWorks`} className="">
-            <p className="nav-btn">How to use</p>
-          </a>
-
-          <a href={`/Faqs`} className="">
-            <p className="nav-btn">FAQ</p>
-          </a>
-
-          <a href={`/Contact`} className="">
-            <p className="nav-btn">Contact</p>
-          </a> */}
         </div>
       </div>
     </nav>
+
+      <div className="mobile-container">
+        <div className="topnav-mobile">
+          <div className="logo-image">
+            <a href="/" className="moneydaddy-link">
+              <img src="MONEYDADDY-2.png" alt="MoneyDaddy" width="25" />
+              <span className="moneydaddy-text">MoneyDaddy</span>
+            </a>
+          </div>
+          <button onClick={showLinks}>
+            <i class="bi bi-list"></i>
+          </button>
+        </div>
+        {showMobileList && (
+          <div className="mobile-list">
+            <a href="/dashboard"> Dashboard</a>
+            <a href="/expense"> Your Expenses </a>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
