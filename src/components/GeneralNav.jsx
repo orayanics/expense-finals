e55ff40063613ec3b87a8d8adffe1987eabd7ae3
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-05-12 23:48:11 +0800

    response navbar

--- src/components/GeneralNav.jsx
+++ src/components/GeneralNav.jsx
@@ -1,18 +1,48 @@
-import '../styles/generalnav.css';
+import { useState } from "react";
+import "../styles/generalnav.css";
 export default function GeneralNav() {
+  const [showMobileList, setShowMobileList] = useState(false);
+
+  const showLinks = () => {
+    setShowMobileList(!showMobileList);
+  };
+
   return (
-    <div className="topnav">
-      <b className="navbar-brand">
-        <div className="logo-image">
-          <a href="/" className="moneydaddy-link">
-            <img src="MONEYDADDY.png" alt="MoneyDaddy" width="25" />
-            <span className="moneydaddy-text">MoneyDaddy</span>
-          </a>
+    <>
+      <div className="topnav">
+        <b className="navbar-brand">
+          <div className="logo-image">
+            <a href="/" className="moneydaddy-link">
+              <img src="MONEYDADDY.png" alt="MoneyDaddy" width="25" />
+              <span className="moneydaddy-text">MoneyDaddy</span>
+            </a>
+          </div>
+        </b>
+        <a href="/how"> HOW IT WORKS</a>
+        <a href="/faqs"> FAQS </a>
+        <a href="/contact"> CONTACT </a>
+      </div>
+
+      {/* MOBILE */}
+      <div className="mobile-container">
+        <div className="topnav-mobile">
+          <div className="logo-image">
+            <a href="/" className="moneydaddy-link">
+              <img src="MONEYDADDY.png" alt="MoneyDaddy" width="25" />
+              <span className="moneydaddy-text">MoneyDaddy</span>
+            </a>
+          </div>
+          <button onClick={showLinks}><i class="bi bi-list"></i></button>
         </div>
-      </b>
-      <a href="/how"> HOW IT WORKS</a>
-      <a href="/faqs"> FAQS </a>
-      <a href="/contact"> CONTACT </a>
-    </div>
+
+        {showMobileList && (
+          <div className="mobile-list">
+            <a href="/how"> HOW IT WORKS</a>
+            <a href="/faqs"> FAQS </a>
+            <a href="/contact"> CONTACT </a>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
